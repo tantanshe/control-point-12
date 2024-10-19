14bc08218feeb5be3e023f89509fcfee7bd95c62
Tatiana Shishenina <tatyana020711@gmail.com>
2024-10-19 14:12:18 +0600

    Created thunks and slice for user entity, configured hooks and store, added types, constant for google login and theme

---
+++ frontend/src/types.ts
@@ -0,0 +1,35 @@
+export interface RegisterMutation {
+  username: string;
+  password: string;
+  displayName: string;
+}
+
+export interface LoginMutation {
+  username: string;
+  password: string;
+}
+
+export interface User {
+  _id: string;
+  username: string;
+  token: string;
+  role: string;
+  displayName?: string;
+  avatar?: string;
+}
+
+export interface ValidationError {
+  errors: {
+    [key: string]: {
+      name: string;
+      message: string;
+    }
+  },
+  message: string;
+  name: string;
+  _message: string;
+}
+
+export interface GlobalError {
+  error: string;
+}
