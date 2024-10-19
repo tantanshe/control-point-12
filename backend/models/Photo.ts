775a1e9554f77718ba8eb447ec28955011156ff0
Tatiana Shishenina <tatyana020711@gmail.com>
2024-10-19 12:25:49 +0600

    Added middleware for authorization and role permission checking, created models for User and Photo entities, added types

---
+++ backend/models/Photo.ts
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const PhotoSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  isPublished: {
+    type: Boolean,
+    default: false,
+    required: true,
+  },
+});
+
+const Photo = mongoose.model('Photo', PhotoSchema);
+
+export default Photo;
---
+++ backend/types.ts
@@ -0,0 +1,16 @@
+export interface UserFields {
+  username: string;
+  password: string;
+  token: string;
+  role: string;
+  displayName?: string;
+  googleID?: string;
+  avatar?: string;
+}
+
+export interface PhotoMutation {
+  title: string;
+  image: string;
+  author: string;
+  isPublished: boolean;
+}
